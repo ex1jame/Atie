c23f843df017c16c6be4ca97ec25f38b0ccc9b54
Temirlan Baysynov <tima.baysynov.2017@gmail.com>
2024-04-03 17:15:07 +0600

    add task orders

--- core/tasks.py
+++ core/tasks.py
@@ -7,5 +7,13 @@ from .celery import app
 def send_confirmation_email_task(user, code):
     send_confirmation_email(user, code)
 
+@app.task
+def send_notification_task(user, order_id,price):
+    send_mail(
+        'Уведомление о создании заказа!',
+        f'Заказ №{order_id} создан, стоимость заказа: {price} рублей',
+        'tima.baysynov.2017@gmail.com',
+        [user],
+    )
 
 
--- order/models.py
+++ order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from core.tasks import send_notification_task
 
 from product.models import Product
 
@@ -32,7 +33,20 @@ class Order(models.Model):
     number = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    total_sum = models.DecimalField(blank=True, decimal_places=2, max_digits=9,default=0.0)
+
+    # @property
+    # def total_sum(self):
+    #     return self.product.aggregate(total_sum=models.Sum('price'))['total_sum']
 
     def __str__(self):
         return f'{self.id} - {self.user}'
 
+@receiver(post_save, sender = Order)
+def order_post_save(sender, instance: Order, *args, **kwargs):
+    send_notification_task.delay(
+        instance.user.email,
+        instance.id,
+        instance.total_sum,
+
+    )
